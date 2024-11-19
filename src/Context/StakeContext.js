097103af2245501aef0c846d6129787dbc81b80a
sundayonah <sundayonah94@gmail.com>
2024-11-19 12:26:17 +0100

    chore: change contract address

--- src/Context/StakeContext.js
+++ src/Context/StakeContext.js
@@ -13,43 +13,43 @@ export const StakingContext = createContext({});
 
 export const StakingContextProvider = ({ children }) => {
    // testnet
-   // const factoryContractAddress = "0x0B440864fe9f47da44E45A193012a60dD73d8062"
-   const factoryContractAddress = "0xEB5663450e6047AB2307e12B74333714976e6705"
+   const factoryContractAddress = "0x0B440864fe9f47da44E45A193012a60dD73d8062"
+   // const factoryContractAddress = "0xEB5663450e6047AB2307e12B74333714976e6705"
 
 
    /// state variables
    const [createdShibbase, setCreatedShibbase] = useState([]);
-     const [isLoading, setIsLoading] = useState(true);
+   const [isLoading, setIsLoading] = useState(true);
 
 
 
    useEffect(() => {
       const fetchFactory = async () => {
-                    setIsLoading(true)
+         setIsLoading(true)
 
          try {
-            
- 
+
+
             const alchemyApiKey = 'https://base-sepolia.g.alchemy.com/v2/k876etRLMsoIcTpTzkkTuh3LPBTK96YZ';
 
-         const provider = new ethers.getDefaultProvider(alchemyApiKey);
-   
-         const contractInstance = new ethers.Contract(
-            factoryContractAddress,
-            factoryAbi,
-            provider
-         );
+            const provider = new ethers.getDefaultProvider(alchemyApiKey);
 
-         const getAllCreatedShibbase = await contractInstance.getAllCreatedShibbase()
+            const contractInstance = new ethers.Contract(
+               factoryContractAddress,
+               factoryAbi,
+               provider
+            );
 
-         const networkDetails = [];
-     
-         // Iterate over each address in the getAllCreatedShibbase array
-         for (let addr of getAllCreatedShibbase) {
-            // Create a new instance of the ERC20 contract for each address
-            const createdShibbase4Address = new ethers.Contract(addr, stakingAbi2, provider);
+            const getAllCreatedShibbase = await contractInstance.getAllCreatedShibbase()
 
-            ////////////
+            const networkDetails = [];
+
+            // Iterate over each address in the getAllCreatedShibbase array
+            for (let addr of getAllCreatedShibbase) {
+               // Create a new instance of the ERC20 contract for each address
+               const createdShibbase4Address = new ethers.Contract(addr, stakingAbi2, provider);
+
+               ////////////
                const aprInSmallestUnits = await createdShibbase4Address.RATE();
                const apr = ethers.formatEther(aprInSmallestUnits);
 
@@ -59,41 +59,41 @@ export const StakingContextProvider = ({ children }) => {
 
                const totalStaker = await createdShibbase4Address.totalStaker();
 
-            ///////////
+               ///////////
 
-            // Fetch the required data from the ERC20 contrac
-            const tokens = await createdShibbase4Address.token();
+               // Fetch the required data from the ERC20 contrac
+               const tokens = await createdShibbase4Address.token();
 
-            const erc20Tokens = new ethers.Contract(tokens, erc20Abi, provider);
-            // console.log(erc20Tokens)
-            
-                  // const balanceOf = await erc20Tokens.balanceOf(signer.getAddress());
-            const name = await erc20Tokens.name();
+               const erc20Tokens = new ethers.Contract(tokens, erc20Abi, provider);
+               // console.log(erc20Tokens)
 
-            const symbol = await erc20Tokens.symbol();
-         
-            
+               // const balanceOf = await erc20Tokens.balanceOf(signer.getAddress());
+               const name = await erc20Tokens.name();
+
+               const symbol = await erc20Tokens.symbol();
+
+
                // Add the network details to the array
-        networkDetails.push({
-          shibAddress: addr,
-          apr: apr,
-          totalStake: formatStake,
-          totalStaker: totalStaker.toString(),
-          tokens: tokens,
-          name: name,
-           symbol: symbol,
-        });
-            setCreatedShibbase(networkDetails)
-                     }
-                  
-                    setIsLoading(false)
+               networkDetails.push({
+                  shibAddress: addr,
+                  apr: apr,
+                  totalStake: formatStake,
+                  totalStaker: totalStaker.toString(),
+                  tokens: tokens,
+                  name: name,
+                  symbol: symbol,
+               });
+               setCreatedShibbase(networkDetails)
+            }
+
+            setIsLoading(false)
          } catch (error) {
             console.error('Error failed fetching data:', error);
-                    
-                 }
+
+         }
       }
       fetchFactory()
-   },[])
+   }, [])
 
 
    return (
